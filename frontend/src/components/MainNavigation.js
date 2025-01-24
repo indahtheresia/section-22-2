370f194ef471c1490e501a8b9e90cbdefcd25f4d
indahtheresia <indahtheresiatheh29@gmail.com>
2025-01-24 13:24:30 +0800

    Practice: project setup

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -1,19 +1,21 @@
+import { NavLink } from 'react-router-dom';
 import classes from './MainNavigation.module.css';
 
 function MainNavigation() {
-  return (
+  return ( <>
     <header className={classes.header}>
       <nav>
         <ul className={classes.list}>
           <li>
-            <a>Home</a>
+            <NavLink to="/" className={({ isActive }) => isActive ? classes.active : undefined} end>Home</NavLink>
           </li>
           <li>
-            <a>Events</a>
+            <NavLink to="/events" className={({ isActive }) => isActive ? classes.active : undefined}>Events</NavLink>
           </li>
         </ul>
       </nav>
     </header>
+  </>
   );
 }
 
