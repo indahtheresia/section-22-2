e63b1c37090607bbfe3f7138e4806b19f829ae7d
indahtheresia <indahtheresiatheh29@gmail.com>
2025-01-24 14:59:31 +0800

    Error handling with custom errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,9 +6,10 @@ import NewEventPage from './pages/NewEventPage';
 import EditEventPage from './pages/EditEventPage';
 import RootLayout from './pages/Root';
 import EventsRootLayout from './pages/EventRoot';
+import ErrorPage from './pages/Error';
 
 const router = createBrowserRouter([
-  { path: '/', element: <RootLayout />, children: [
+  { path: '/', element: <RootLayout />, errorElement: <ErrorPage />, children: [
     { index: true, element: <HomePage /> },
     { path: 'events', element: <EventsRootLayout />, children: [
       { index: true, 
