0fd16c745a0eedd58812ea461184301747ceb38e
indahtheresia <indahtheresiatheh29@gmail.com>
2025-01-27 09:48:44 +0800

    Submit data programmatically

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import EventsPage, { loader as eventsLoader } from './pages/EventsPage';
-import EventDetailPage, { loader as eventDetailsLoader} from './pages/EventDetailPage';
+import EventDetailPage, { loader as eventDetailsLoader, action as deleteEventAction } from './pages/EventDetailPage';
 import NewEventPage, { action as newEventAction } from './pages/NewEventPage';
 import EditEventPage from './pages/EditEventPage';
 import RootLayout from './pages/Root';
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
         loader: eventsLoader,
       },
       { path: ':eventId', id: 'event-detail', loader: eventDetailsLoader, children:[
-        { index: true, element: <EventDetailPage />, },
+        { index: true, element: <EventDetailPage />, action: deleteEventAction },
         { path: 'edit', element: <EditEventPage /> },
       ]},
       
--- frontend/src/pages/EventDetailPage.js
+++ frontend/src/pages/EventDetailPage.js
@@ -1,4 +1,4 @@
-import { useRouteLoaderData } from "react-router-dom";
+import { redirect, useRouteLoaderData } from "react-router-dom";
 import EventItem from '../components/EventItem';
 
 function EventDetailPage() {
@@ -22,3 +22,15 @@ export async function loader({request, params}) {
     return response;
   }
 };
+
+export async function action({params, request}) {
+  const eventId = params.eventId;
+  const response = await fetch('http://localhost:8080/events/' + eventId, {
+    method: request.method,
+  });
+
+  if (!response.ok) {
+    throw new Response(JSON.stringify({ message: 'Could not delete event.'}), { status: 500 });
+  }
+  return redirect('/events');
+}
