036f28492ab69d8d4e7cf881f8bc4ee14166db45
indahtheresia <indahtheresiatheh29@gmail.com>
2025-01-27 15:10:17 +0800

    Defer data fetching

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,19 +1,24 @@
-import { useLoaderData } from 'react-router-dom';
+import { Await, useLoaderData } from 'react-router-dom';
 import EventsList from '../components/EventsList';
+import { Suspense } from 'react';
 
 function EventsPage() {
-  const data = useLoaderData();
+  const { events } = useLoaderData();
   // if (data.isError) {
   //  return <p>{data.message}
   // }
-  const events = data.events;
+  // const events = data.events;
 
-  return <EventsList events={events} />;
+  return (
+    <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+      <Await resolve={events}>{(loadedEvents) => <EventsList events={loadedEvents} />}</Await>
+    </Suspense>
+  );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
   const response = await fetch('http://localhost:8080/events');
 
   if (!response.ok) {
@@ -22,6 +27,13 @@ export async function loader() {
     throw new Response(JSON.stringify({ message: 'Could not fetch events.'}), { status: 500});
     
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+};
+
+export async function loader() {
+  return { 
+    events: loadEvents(),
+  };
 }
